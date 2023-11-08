c282a2371bb12ce8e66fa85f8f70b9c10d1d69fe
heliko0 <144160130+heliko0@users.noreply.github.com>
2023-11-08 21:38:21 +0000

    Added additional buttons to switch colours
    Added functionality to allow user to switch between greyscale and rgb
    cell colours.

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ const button = document.createElement("button"); //add input button
 button.textContent = "New grid";
 document.body.appendChild(button);
 
+const colourSelect = document.querySelector("#buttons");
+
+const makeRGB = document.createElement("button");
+makeRGB.textContent = "Spice it up!";
+colourSelect.appendChild(makeRGB);
+
+const makeBlack = document.createElement("button");
+makeBlack.textContent = "Boo!";
+colourSelect.appendChild(makeBlack);
+
+
+// let user clear grid and add custom amount of pixels
 button.addEventListener("click", () => {
-    // let user clear grid and add custom amount of pixels
     let squares = parseInt(prompt("How many squares do you want?"));
 
     if (!Number.isInteger(squares) || squares > 100) {
-        alert("Sorry, I can't do that. Try again.");
-        // error if over 100 pixels or not integer
+        alert("Sorry, I can't do that. Try again."); // error if over 100 pixels or not integer
     } else {
         while (container.firstChild) {
-            container.removeChild(container.firstChild);
-            // while container has firstChild (cells) then remove said cell
+            container.removeChild(container.firstChild); // while container has firstChild (cells) then remove said cell
         }
         createGrid(squares); // create new grid with user inputted pixels
     }
@@ -23,13 +32,12 @@ const containerWidth = container.clientWidth;
 const containerHeight = container.clientHeight;
 //get width and height of container from CSS
 
-createGrid(16); // on load make 16x16 grid
+document.addEventListener("DOMContentLoaded", createGrid(16)); // on load make 16x16 grid
 
 function createGrid(x) {
     for (let rows = 0; rows < x; rows++) {
         //adds 16x16 grid
         for (let columns = 0; columns < x; columns++) {
-
             const cell = document.createElement("div");
             cell.className = "cell";
             cell.style.width = `${containerWidth / x}px`;
@@ -44,7 +52,8 @@ function createGrid(x) {
             let rgba = `rgba(${r},${g},${b},${a})`; //converts to rgba value from strings
             cell.style.backgroundColor = `${rgba}`; //sets rgba value
 
-            cell.dataset.rgba = rgba; //stores rgba value in dataset
+            cell.dataset.OGrgba = rgba; //stores original rgba value in dataset for later
+            cell.dataset.rgba = rgba; // stores current rgba value in dataset
             cell.dataset.opacity = 0; //stores opacity in dataset
 
             cell.addEventListener("mouseover", darken); // on hover, activate darken function
@@ -56,14 +65,41 @@ function createGrid(x) {
 
 function darken(event) {
     let cell = event.target;
-    let currentOpacity = parseFloat(cell.dataset.opacity); // gets opacity from dataset and stores in new variable. parseFloat needed due to datasets being strings
-    let newOpacity = currentOpacity + 0.1;  // opacity incremented by 0.1 10%
-    let newRGBA = cell.dataset.rgba;    // gets rgba from dataset and stores in new variable. parseFloat not needed as we use string methods 
 
-    newRGBA = newRGBA.replace(currentOpacity, newOpacity);  // converts opacity values from floats to strings and replaces old opacity value to new one
+    let newOpacity = parseFloat(cell.dataset.opacity) + 0.1; // gets opacity from dataset. parseFloat needed due to datasets being strings. Increments by 10%
+    if (newOpacity > 1) newOpacity = 1; // ensure opacity does not exceed 100%
 
-    cell.style.backgroundColor = newRGBA;   // update CSS with darkened rgba value
-    cell.dataset.opacity = newOpacity;  // updates opacity dataset
-    cell.dataset.rgba = newRGBA;    // updates rgba dataset
+    let newRGBA = (cell.dataset.rgba).replace(/\d+(\.\d+)?(?=\))/, newOpacity.toFixed(1)); 
+    // gets rgba from dataset, parseFloat not needed as we use string methods. 
+    // uses regular expression to search. searches for digit, (optionally) followed by decimal point and then 1 or more digits (eg 0.5), and then followed by closing brackets. This makes sure to select the alpha value. converts newOpacity from float to string and replaces. 
+
+    cell.style.backgroundColor = newRGBA; // update CSS with darkened rgba value
+    cell.dataset.rgba = newRGBA; // updates rgba dataset
+    cell.dataset.opacity = newOpacity; // updates opacity dataset
 }
 
+makeRGB.addEventListener("click", () => {
+    const cells = document.querySelectorAll(".cell");  
+
+    cells.forEach((cell) => {
+        let currentOpacity = parseFloat(cell.dataset.opacity);
+        let newRGBA = cell.dataset.OGrgba; // gets original assigned rgba value
+
+        newRGBA = newRGBA.replace(/\d+(\.\d+)?(?=\))/, currentOpacity); // resets to original value, just keeping opacity
+
+        cell.style.backgroundColor = newRGBA;
+        cell.dataset.rgba = newRGBA; 
+    });
+});
+
+makeBlack.addEventListener("click", () => {
+    const cells = document.querySelectorAll(".cell");
+
+    cells.forEach((cell) => {
+        let currentOpacity = parseFloat(cell.dataset.opacity);
+        let newRGBA = `rgba(0,0,0,${currentOpacity})`; // changes to greyscale, just keeping opacity
+
+        cell.style.backgroundColor = newRGBA;
+        cell.dataset.rgba = newRGBA;
+    });
+});
